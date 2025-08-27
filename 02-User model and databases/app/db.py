ab2ffaac2b27623a5253724cc0f7b8538610e54b
acelee0621 <acelee0621@outlook.com>
2025-08-27 17:32:51 +0800

    ch.2 数据库与适配器

---
+++ 02-User model and databases/app/db.py
@@ -0,0 +1,20 @@
+# app/db.py
+import motor.motor_asyncio
+from beanie import Document
+from fastapi_users.db import BeanieBaseUser, BeanieUserDatabase
+
+
+DATABASE_URL = "mongodb://localhost:27017"
+
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    DATABASE_URL, uuidRepresentation="standard"
+)
+db = client["user_demo"]
+
+
+class User(BeanieBaseUser, Document):
+    pass
+
+
+async def get_user_db():
+    yield BeanieUserDatabase(User)
